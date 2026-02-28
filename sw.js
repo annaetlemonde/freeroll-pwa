cdf5adf17dcd88a45b6188534ea40201b7e079b0
annaetlemonde <44240463+annaetlemonde@users.noreply.github.com>
2026-02-28 00:40:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Freeroll Poker PWA
 // IMPORTANT: bump CACHE_NAME every time you update index.html
-const CACHE_NAME = "freeroll-cache-v8";
+const CACHE_NAME = "freeroll-cache-v9";
 
 const ASSETS = [
   "./",
