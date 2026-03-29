927871eaf9932f5eaf76a5ba81408b8e306635ed
annaetlemonde <44240463+annaetlemonde@users.noreply.github.com>
2026-03-29 18:13:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js — 5D PWA
 // IMPORTANT: bump CACHE_NAME every time you update index.html
 
-const CACHE_NAME = "5d-cache-v57";
+const CACHE_NAME = "5d-cache-v58";
 
 const ASSETS = [
   "./",
